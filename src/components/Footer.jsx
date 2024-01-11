a7bf59e5fffd3920810e0dec1be72851d45bfedb
joellesenne <jfkles@me.com>
2024-01-11 22:42:50 +0100

    🐛 fix(package.json): fix lint:fix script command to include double dash before fix flag ✨ feat(Footer.jsx): add "All rights reserved" text to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p>
-                © {useGetYear()}{" "}
+                © {useGetYear()}{" | "}
+                All rights reserved{" | "}
                 <a
                     href="https://joellesenne.dev"
                     rel="noopener noreferrer"
